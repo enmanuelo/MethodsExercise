19e02b137f6ce62dc57471a5ab0a9fe06cbe413a
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-05 12:22:42 -0400

    cleaned up comments

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -28,17 +28,17 @@ namespace MethodsExercise
         {
             //exercise 1
 
-            Console.WriteLine("What is your name?"); //output
-            var userName = Console.ReadLine(); //input
+            Console.WriteLine("What is your name?"); 
+            var userName = Console.ReadLine(); 
 
-            Console.WriteLine("Where do you live?"); //output
-            var userCity = Console.ReadLine(); //input
+            Console.WriteLine("Where do you live?"); 
+            var userCity = Console.ReadLine();
 
-            Console.WriteLine("What color is your house?"); //output
-            var userHouse = Console.ReadLine(); //input
+            Console.WriteLine("What color is your house?"); 
+            var userHouse = Console.ReadLine(); 
 
-            Console.WriteLine("What car do you drive?"); //output
-            var userCar = Console.ReadLine(); //input
+            Console.WriteLine("What car do you drive?");
+            var userCar = Console.ReadLine(); 
 
             Console.WriteLine($"Hi! My name is {userName}. I live in a {userHouse} house in {userCity} and drive a {userCar}.");
 
