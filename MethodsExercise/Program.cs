897a98cbd8dc33c07f609d9bde8991cc738cb3b5
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-05 12:08:14 -0400

    created short story and calculator methods

---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MethodsExercise2
+{
+    class Program
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        static void Main(string[] args)
+        {
+            //exercise 1
+
+            Console.WriteLine("What is your name?"); //output
+            var userName = Console.ReadLine(); //input
+
+            Console.WriteLine("Where do you live?"); //output
+            var userCity = Console.ReadLine(); //input
+
+            Console.WriteLine("What color is your house?"); //output
+            var userHouse = Console.ReadLine(); //input
+
+            Console.WriteLine("What car do you drive?"); //output
+            var userCar = Console.ReadLine(); //input
+
+            Console.WriteLine($"Hi! My name is {userName}. I live in a {userHouse} house in {userCity} and drive a {userCar}.");
+
+
+            //exercise 2
+
+            var numsAdded = Add(3, 5);
+            var numsSubtracted = Subtract(4, 2);
+            var numsMultiplied = Multiply(2, 4);
+            var numsDivided = Divide(9, 3);
+
+        }
+        
+    }
+
+}
